f87cb95c91c45047384139403e3ef9ce3c46bbc4
ghina <baassiri.ghina@gmail.com>
2024-08-05 00:27:29 +0300

    Add new endpoint

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const app = express();
-require('dotenv').config();
+// require('dotenv').config();
+const xlsx = require('xlsx');
 
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 3000;
+
+const wb = xlsx.readFile('students.xlsx');
+const ws = wb.Sheets["data"]
 
 // your code
 app.get('/', function (req,res) {
     res.send('hello world');
+})
+
+app.get('/students', function (req,res) {
+    var data = xlsx.utils.sheet_to_json(ws)
+    res.send(data);
 })
 
 app.listen(PORT, () => {
